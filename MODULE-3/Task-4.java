184a53886290bc6679513febcde0aca91238359a
6439959_Bodi-Tejaswini <tejuteja088@gmail.com>
2025-05-28 15:30:57 +0530

    Task-4.java

--- MODULE-3/Task-4.java
+++ MODULE-3/Task-4.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class que4_LeapYearChecker {
+public class LeapYearChecker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Prompt the user to enter a year
+        
         System.out.print("Enter a year: ");
         int year = sc.nextInt();
 
-        // Apply leap year logic
+        
         if (year % 4 == 0) {
             if (year % 100 != 0 || year % 400 == 0) {
                 System.out.println(year + " is a Leap Year.");
